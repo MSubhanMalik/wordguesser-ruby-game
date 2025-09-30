180f0c77f2f3116124bd22f8dd2ffc7de8aed36b
Muhammad Subhan <msubhanmalik20@gmail.com>
2025-10-01 03:54:01 +0500

    Changes

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -8,25 +8,21 @@ class WordGuesserGame
   attr_accessor :word, :guesses, :wrong_guesses
 
   def guess(letter)
-    # Validate input - raise ArgumentError for nil or empty
     raise ArgumentError, 'Invalid guess.' if letter.nil? || letter.empty?
     
     letter = letter.downcase
     
-    # Check if it's a valid letter (a-z only)
     raise ArgumentError, 'Invalid guess.' unless letter.match?(/[a-z]/)
     
-    # Check if already guessed - return false (not an error)
     return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
     
-    # Process the valid new guess
     if @word.include?(letter)
       @guesses += letter
     else
       @wrong_guesses += letter
     end
     
-    true  # Return true for successful new guess
+    true  
   end
 
   def word_with_guesses
